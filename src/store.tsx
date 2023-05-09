bb87b8d8bca0489a057e2a190aaae40592e8f8ed
kriss57 <kriss@DESKTOP-KTQLM77.localdomain>
2023-05-09 08:39:36 +0200

    move the calls of requests in the store

--- src/store.tsx
+++ src/store.tsx
@@ -1,10 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userSlice } from "./slices/userSlice";
+import requestSlice from "./slices/requestSlice";
 
 export const store = configureStore({
   reducer: {
     userInfo: userSlice.reducer,
+    request: requestSlice,
   },
 });
 
 export type AppDispatch = typeof store.dispatch;
+export type RootState = {
+  userInfo: {
+    firstName: string;
+    lastName: string;
+    checked: boolean;
+  };
+  request: {
+    isLoading: boolean;
+    error: string | null;
+  };
+};
