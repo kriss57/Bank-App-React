28a81d268e5dd077de020a0ebcf7b8c85cfb4b18
kriss57 <kriss@DESKTOP-KTQLM77.localdomain>
2023-05-09 08:42:51 +0200

    create new slice component for calls of requests

---
+++ src/slices/requestSlice.tsx
@@ -0,0 +1,148 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { requestService } from "../_services/request.service";
+
+interface UserProfile {
+  firstName: string;
+  lastName: string;
+}
+
+interface loginForm {
+  email: string;
+  password: string;
+}
+
+export const fetchLogin = createAsyncThunk(
+  "request/fetchLogin",
+  async (formData: loginForm, thunkAPI) => {
+    const response = await requestService.login(formData);
+    return response.data.body.token;
+  }
+);
+
+// D'abord, créer le thunk
+export const fetchGetUser = createAsyncThunk(
+  "request/fetchGetUser",
+  // pas de premier argument , utilisé underscore(-, )
+  async (_, thunkAPI) => {
+    try {
+      const response = await requestService.getUser();
+      console.log(response);
+
+      let firstName = response.data.body.firstName;
+      let lastName = response.data.body.lastName;
+
+      // Feeds the store userInfo
+      thunkAPI.dispatch({
+        type: "userInfo/setFirstName",
+        payload: firstName,
+      });
+      thunkAPI.dispatch({
+        type: "userInfo/setLastName",
+        payload: lastName,
+      });
+    } catch (error) {
+      console.error(error);
+      throw new Error("Error get user data");
+    }
+  }
+);
+
+interface updateForm {
+  firstName: string;
+  lastName: string;
+}
+export const fetchUpdateUser = createAsyncThunk(
+  "request/fetchUpdateUser",
+  async (formData: updateForm, thunkAPI) => {
+    try {
+      if (!formData.firstName || !formData.firstName) {
+        throw new Error("The fiels is empty");
+      }
+      const response = await requestService.updateUser(formData);
+
+      let firstName = response.data.body.firstName;
+      let lastName = response.data.body.lastName;
+
+      // Update the store userInfo
+      thunkAPI.dispatch({
+        type: "userInfo/setFirstName",
+        payload: firstName,
+      });
+      thunkAPI.dispatch({
+        type: "userInfo/setLastName",
+        payload: lastName,
+      });
+      //return response.data;
+    } catch (error) {
+      throw new Error("Error update user data");
+    }
+  }
+);
+
+interface UserState {
+  data: UserProfile | null;
+  isLoading: boolean;
+  error: string | null;
+}
+
+const initialState: UserState = {
+  data: null,
+  isLoading: false,
+  error: null,
+};
+
+// Ensuite, gérez les actions dans vos réducteurs :
+const requestSlice = createSlice<UserState, any>({
+  name: "request",
+  initialState,
+  reducers: {
+    // logique standard des réducteurs, avec des types d'action auto-générés par réducteur
+  },
+  extraReducers: (builder) => {
+    //Ajouter des reducers pour des types d'actions supplémentaires ici, et gérer l'état de chargement si nécessaire
+    builder
+      //la promesse commence à etre résolue => isLoadind passe à true
+      .addCase(fetchLogin.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      //la promesse est résolue => on peu passer isLoading a false
+      .addCase(fetchLogin.fulfilled, (state, action) => {
+        state.isLoading = false;
+      })
+      //la promesse et rejeté => on peu passer isLoading à false et on stock le message d'érreur dans le state
+      .addCase(fetchLogin.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message as string;
+      });
+    builder
+      // la promesse commence à etre résolue => isLoadind passe à true
+      .addCase(fetchGetUser.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      // la promesse est résolue => on peu passer isLoading a false
+      .addCase(fetchGetUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+      })
+      // la promesse et rejeté => on peu passer isLoading à false et on stock le message d'érreur dans le state
+      .addCase(fetchGetUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message as string;
+      });
+    builder
+      //la promesse commence à etre résolue => isLoadind passe à true
+      .addCase(fetchUpdateUser.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      //la promesse est résolue => on peu passer isLoading a false
+      .addCase(fetchUpdateUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+      })
+      //la promesse et rejeté => on peu passer isLoading à false et on stock le message d'érreur dans le state
+      .addCase(fetchUpdateUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message as string;
+      });
+  },
+});
+
+export default requestSlice.reducer;
